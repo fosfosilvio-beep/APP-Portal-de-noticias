3e719c43c9394d8d1bc5c8366b5e43b5d51f9fd3
fosfosilvio-beep <fosfosilvio@gmail.com>
2026-05-03 16:58:00 -0300

    Fix: TypeScript implicit any error for 'session' in CommentsSection.tsx

--- portal-noticias/src/components/noticias/CommentsSection.tsx
+++ portal-noticias/src/components/noticias/CommentsSection.tsx
@@ -33,7 +33,7 @@ export default function CommentsSection({ noticiaId }: CommentsSectionProps) {
     checkUser();
 
     // Listen for auth changes
-    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event: any, session) => {
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event: any, session: any) => {
       if (session?.user) {
         setUser(session.user);
         setNome(session.user.user_metadata?.full_name || session.user.user_metadata?.name || session.user.email?.split('@')[0] || "");
